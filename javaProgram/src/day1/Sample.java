8d44fba2fb4430723edfb0eabb32fe3c1d26758a
acer <acer@LAPTOP-TFSCGK6T>
2024-07-01 16:31:57 +0530

    java project

---
+++ javaProgram/src/day1/Sample.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.print("java program");
+		
+
+	}
+
+}
