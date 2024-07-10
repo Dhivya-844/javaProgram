233f134902848d97460f0428b3d6e54d530a0b7b
acer <acer@LAPTOP-TFSCGK6T>
2024-07-10 14:41:06 +0530

    one

---
+++ javaProgram/src/day6/EmployeeDemo.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Employee.companyName");
+		Employee e=new Employee("dhivya",101);
+		
+		System.out.println(e);
+		
+		e = new Employee("Deep", 321);
+		System.out.println(e);
+
+	}
+
+}
